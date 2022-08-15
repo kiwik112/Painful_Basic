8f85e750686e9e711282e26a39e4af9404e0eeee
kiwik112 <pakiwi06@gmail.com>
2022-08-15 22:24:18 +0200

    added error handling

--- main.py
+++ main.py
@@ -17,18 +17,26 @@ async def pain(ctx) :
     global channel
     if channel == "" :
         channel = ctx.channel
-        await ctx.send("``` * * * PAINFUL BASIC ALPHA 3 * * *\rA SHITTON BYTES FREE\rNO COPYRIGHT 2022```")
+        await ctx.send("``` * * * PAINFUL BASIC ALPHA 6 * * *\rA SHITTON BYTES FREE\rNO COPYRIGHT 2022```")
         return
     await ctx.send("Already being used elsewhere.")
 
 @client.command(aliases = [ "PRINT", "$" ])
 async def printFn(ctx, *, toprint) :
-    toprint += "f"
-    toprint = toprint.replace("\\\"", "#!")
-    printing = toprint.split("\"")
-    if not printing.length == 3 :
+    if toprint.isupper() :
+        toprint += "f"
+        toprint = toprint.replace("\\\"", "#!")
+        printing = toprint.split("\"")
+        if not len(printing) == 3 :
+            await ctx.send("?SYNTAX ERROR")
+            return
+        await ctx.send(printing[1].replace("#!", "\""))
+    else :
+        await ctx.send("?CHARACTER ERROR")
+
+@client.event
+async def on_command_error(ctx, error) :
+    if channel == ctx.channel and ctx.message.content.isupper() :
         await ctx.send("?SYNTAX ERROR")
-        return
-    await ctx.send(printing[1].replace("#!", "\""))
 
 client.run(token)
